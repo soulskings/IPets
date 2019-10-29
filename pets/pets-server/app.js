39c459820cc9b324944e0f838d31079db3da3807
qianyuchen <qianyuchen@9fbank.com.cn>
2019-10-29 15:47:27 +0800

    feat(pets-server):添加链接数据库逻辑及依赖

--- pets/pets-server/app.js
+++ pets/pets-server/app.js
@@ -1,32 +1,79 @@
-require('babel-core/register');
-require('babel-polyfill');
+require('babel-core/register')
+require('babel-polyfill')
 
+const fs = require('fs')
+const path = require('path')
+const mongoose = require('mongoose')
 
-const Koa = require('koa');
-const cors = require('koa2-cors');
-// const bodyParser = require('koa-bodyparser');
+const db = 'mongodb://localhost/test'
+
+/**
+ * mongoose连接数据库
+ * @type {[type]}
+ */
+mongoose.Promise = require('bluebird')
+mongoose.connect(db)
+
+/**
+ * 获取数据库表对应的js对象所在的路径
+ * @type {[type]}
+ */
+const models_path = path.join(__dirname, '/models')
+
+/**
+ * 已递归的形式，读取models文件夹下的js模型文件，并require
+ * @param  {[type]} modelPath [description]
+ * @return {[type]}           [description]
+ */
+var walk = function(modelPath) {
+    fs
+    .readdirSync(modelPath)
+    .forEach(function(file) {
+        var filePath = path.join(modelPath, '/' + file)
+        var stat = fs.statSync(filePath)
+
+        if (stat.isFile()) {
+            if (/(.*)\.(js|coffee)/.test(file)) {
+            require(filePath)
+            }
+        }
+        else if (stat.isDirectory()) {
+            walk(filePath)
+        }
+    })
+}
+walk(models_path)
+
+const Koa = require('koa')
+const logger = require('koa-logger')
+const session = require('koa-session')
+const cors = require('koa2-cors')
+const bodyParser = require('koa-bodyparser')
 var body = require('koa-body')
 
-const controller = require('./controller');
+const controller = require('./controller')
 
-const app = new Koa();
+const app = new Koa()
 
-app.use(cors());
+app.use(logger())
+app.use(cors())
+app.use(session(app))
+app.use(bodyParser())
 
 // log request URL:
 app.use(async (ctx, next) => {
     
-    await next();
+    await next()
     
-    console.log(`Process ${ctx.request.method} ${ctx.request.url} ...`);
+    console.log(`Process ${ctx.request.method} ${ctx.request.url} ...`)
 });
 
 
 // parse request body:
-app.use(body());
+app.use(body())
 
 // add controllers:
-app.use(controller());
+app.use(controller())
 
-app.listen(3001);
-console.log('app started at port 3001...');
+app.listen(3001)
+console.log('app started at port 3001...')
