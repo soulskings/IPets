d0a1cc94b0943416a7aef4694cb30496459848d9
qianyuchen <qianyuchen@9fbank.com.cn>
2019-12-15 14:49:08 +0800

    feat(pets-server、register.js):修改数据库地址

--- pets/pets-server/controllers-modules/register/register.js
+++ pets/pets-server/controllers-modules/register/register.js
@@ -1,19 +1,10 @@
 const CircularJSON = require('circular-json');
 const registerController = async (ctx, next) => {
-  console.log(ctx)
-  let body
-  if (ctx.header.mobile) {
-    body = await CircularJSON.stringify({
+  const  body = await CircularJSON.stringify({
         code: '000000',
         token: '111111111',
         message: '成功'
     })
-  } else {
-    body = await CircularJSON.stringify({
-      code: '000031',
-      message: '未登录'
-    })
-  }
   ctx.response.body = body
 }
 
