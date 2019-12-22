0983644f4750a3a69ed8c56a69fd0d103240f909
qianyuchen <qianyuchen@9fbank.com.cn>
2019-12-22 12:47:13 +0800

    feat(replace-token、code):添加参数异常错误码以及错误逻辑

--- pets/pets-server/controllers-modules/login/replace-token.js
+++ pets/pets-server/controllers-modules/login/replace-token.js
@@ -1,8 +1,9 @@
 const CustomError = require('../../utils/error-class.js')
+const { template } = require('../../api/code')
 const replaceTokenController = async (ctx, next) => {
   let body
   if (!ctx.request.body.code) {
-    throw new CustomError()
+    throw new CustomError(template.paramsError)
   } else {
     body = await {
         code: '000000',
