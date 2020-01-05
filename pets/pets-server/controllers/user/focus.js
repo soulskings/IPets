124b9c428d947896b10a847cb2f0d9a69ac3139f
qianyuchen <qianyuchen@9fbank.com.cn>
2020-01-05 21:50:42 +0800

    feat(关注):修改查询宠物及文章方式

--- pets/pets-server/controllers/user/focus.js
+++ pets/pets-server/controllers/user/focus.js
@@ -1,28 +1,41 @@
 // 收藏
 const CustomError = require('@utils/error-class.js')
 const { template, resCode, message } = require('@api/code')
-const {focusPet, cancleFocusPet} = require('../../dbheleper/user/focus-pet-action')
-const {focusArticle, cancleFocusArticle} = require('../../dbheleper/user/focus-article-action')
+const { focusPet, cancleFocusPet } = require('../../dbheleper/user/focus-pet-action')
+const { focusArticle, cancleFocusArticle } = require('../../dbheleper/user/focus-article-action')
+const queryPet = require('../../dbheleper/user/query-pet')
+const queryArticle = require('../../dbheleper/user/query-article')
 
 const focusController = async (ctx, next) => {
   const openid = ctx.header.openid
   const body = ctx.request.body
-  const { type, cid, pid, tag, name, img_url } = body
-  const { title, intro, article_img, article_type, article_id } = body
+  const { type, pid } = body
+  const { article_type, article_id } = body
   let focusData = {}
   if (!openid || !type) {
     throw new CustomError(template.paramsError)
   }
   // type: 1收藏宠物 2收藏文章
   if (type == 1 && pid) {
-    // 收藏宠物
-    focusData = {
-      openid,
-      cid,
-      pid,
-      name,
-      img_url,
-      tag
+    // 查询宠物列表库中收藏宠物的信息
+    const pet = await queryPet(pid)
+    const { cid, name, img_url, tag } = pet
+    if (pet) {
+      // 收藏宠物
+      focusData = {
+        openid,
+        cid,
+        pid,
+        name,
+        img_url,
+        tag
+      }
+    } else {
+      throw new CustomError({
+        code: resCode.ERROR,
+        data: null,
+        message: 'pid不存在, 无此宠物'
+      })
     }
     const status = await focusPet(focusData)
     
@@ -35,14 +48,25 @@ const focusController = async (ctx, next) => {
     }
     
   } else if (type === '2' && article_id) {
-    // 收藏文章
-    focusData = {
-      openid,
-      title,
-      intro,
-      article_img,
-      article_type,
-      article_id
+    // 查询文章列表中此文章信息
+    const article = await queryArticle()
+    const { title, intro, article_img } = article
+    if (article) {
+      // 收藏文章
+      focusData = {
+        openid,
+        title,
+        intro,
+        article_img,
+        article_type,
+        article_id
+      }
+    } else {
+      throw new CustomError({
+        code: resCode.ERROR,
+        data: null,
+        message: 'article_id不存在, 无此文章'
+      })
     }
     const status = await focusArticle(focusData)
 
