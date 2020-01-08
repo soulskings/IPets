8b81d1f1e9cf2e46a6b7f7d54abf2b97a567efc9
wupeng <wupeng@uctimes.cn>
2020-01-08 21:47:04 +0800

    添加首页及公共文件

---
+++ pets/pets-mp/http/requesUrl.js
@@ -0,0 +1,64 @@
+/**
+ * 后台接口配置文件
+ */
+// import env from './env.js'
+let requesUrl = {
+	/**
+	 * 获取token接口
+	 */
+	replaceToken: 'pets/replace-token',
+	/**
+	 * 退出登录接口
+	 */
+	petsList : 'pets/quit',
+	/**
+	 * 宠物种类列表接口
+	 */
+	petsCate : 'pets/cate',
+	/**
+	 * 全部宠物列表接口
+	 */
+	petsList : 'pets/list',
+	/**
+	 * 热门宠物列表接口
+	 */
+	petsHot : 'pets/hot',
+	/**
+	 * 宠物详情接口
+	 */
+	petsDetail : 'pets/detail',
+	/**
+	 * 文章接口
+	 */
+	petsArticleDetail : 'pets/articleDetail',
+	/**
+	 * 热搜接口
+	 */
+	petsHotSearch : 'pets/hotsearch',
+	/**
+	 * 搜索宠物列表接口
+	 */
+	petsSearchPetList : 'pets/searchpetlist',
+	/**
+	 * 搜索文章列表接口
+	 */
+	petsSearchArticle : 'pets/searcharticle',
+	/**
+	 * 收藏列表接口
+	 */
+	petsFocuspets : 'pets/focuspets',
+	/**
+	 * 收藏文章列表接口
+	 */
+	petsFocusarticle : 'pets/focusarticle',
+	/**
+	 * 收藏接口
+	 */
+	petsFocus : 'pets/focus',
+	/**
+	 * 取消收藏接口
+	 */
+	petsCanclefocus : 'pets/canclefocus'
+}
+
+export default requesUrl;
---
+++ pets/pets-mp/pages/index/components/classification.js
@@ -0,0 +1,92 @@
+// pages/index/components/classification.js
+import {
+  getPetsCate
+} from '../../../http/index.js'
+import {
+  resCode
+} from '../../../utils/code.js'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    srcList: [{
+        img: '/static/images/8.jpg',
+        cid: '0',
+        name: '狗'
+      },
+      {
+        img: '/static/images/8.jpg',
+        cid: '1',
+        name: '猫'
+      },
+      {
+        img: '/static/images/8.jpg',
+        cid: '2',
+        name: '兔子'
+      },
+      {
+        img: '/static/images/8.jpg',
+        cid: '3',
+        name: '鼠'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
