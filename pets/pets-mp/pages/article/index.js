a4c479549ccbd212a462d74b383658cf5304fd58
pucklee <liyibo@taou.com>
2020-01-17 22:10:57 +0800

    收藏

--- pets/pets-mp/pages/article/index.js
+++ pets/pets-mp/pages/article/index.js
@@ -16,10 +16,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let { articleId, articleType } = options
+    let { article_id, article_type } = options
     this.fetchArticleData({
-      articleId,
-      articleType
+      articleId: article_id,
+      articleType: article_type
     })
   },
 
