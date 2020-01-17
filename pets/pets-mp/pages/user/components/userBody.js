1a43253318eaf60f75b5fb26299f5848f0a52a38
wupeng <wupeng@uctime.cn>
2020-01-17 22:10:06 +0800

    添加跳转

--- pets/pets-mp/pages/user/components/userBody.js
+++ pets/pets-mp/pages/user/components/userBody.js
@@ -18,6 +18,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    goCollect() {
+      console.log(1111)
+      wx.navigateTo({
+        url: '/pages/my-collections/index'
+      })
+    }
   }
 })
