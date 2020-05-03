1ebd9c02facc4727d29e443bd31aa0f4b697f22b
qianyuchen <qianyuchen@9fbank.com.cn>
2020-05-03 15:13:00 +0800

    feat(config):上线

--- pets/pets-server/config/index.js
+++ pets/pets-server/config/index.js
@@ -1,8 +1,8 @@
 const URI = {
   MONGODB: {
-    pro: '',
+    pro: 'mongodb://182.92.86.190:27017',
     test: 'mongodb://285471z3f6.qicp.vip:29731',
-    dev: 'mongodb://192.168.8.196:27017'
+    dev: 'mongodb://127.0.0.1:27017'
   }
 }
 
